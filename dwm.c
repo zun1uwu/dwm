01c4d3bddb4ba1743a04b7a7ed55911683d5c0ed
zun1 <zun1@duck.com>
2024-03-30 18:51:46 +0100

    remove dwm version from bar

--- dwm.c
+++ dwm.c
@@ -2244,7 +2244,7 @@ void
 updatestatus(void)
 {
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
-		strcpy(stext, "dwm-"VERSION);
+		strcpy(stext, "");
 	drawbar(selmon);
 	updatesystray();
 }
